7de25582816f36c3109bda02823df2057ad5fdc2
Vidit Shah <vidit0210@protonmail.com>
2025-06-10 16:30:47 +0530

    updated Video Url

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -148,7 +148,7 @@ function HeroImage() {
     >
       <HeroVideoDialog
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/qh3NGpYRG3I?si=4rb-zSdDkVK9qxxb"
+        videoSrc="https://www.youtube.com/embed/J5Dk-eize_0"
         thumbnailSrc="/dashboard.png"
         thumbnailAlt="Hero Video"
         className="border rounded-lg shadow-lg max-w-screen-lg mt-16"
